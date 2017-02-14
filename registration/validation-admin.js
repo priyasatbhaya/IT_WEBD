5390624d50406a8edd616c382c7eae6bb8346002
priyasatbhaya <priyasatbhaya64@gmail.com>
2017-02-15 01:06:50 +0530

    Added autofill in both registration forms.

--- registration/validation-admin.js
+++ registration/validation-admin.js
@@ -25,3 +25,39 @@ function validateForm() {
         return false;
     }
 }
+
+function getAge(){
+    var dateString = document.forms["admin_signup_form"]["dob"].value;
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    document.forms["admin_signup_form"]["age"].value = age;
+}
+
+function getAdminid() {
+    var dpt = document.forms["admin_signup_form"]["list"].value;
+    if(dpt=="bt")
+        document.forms["admin_signup_form"]["adminid"].value = "14/BT/";
+    if(dpt=="che")
+        document.forms["admin_signup_form"]["adminid"].value = "14/CHE/";
+    if(dpt=="civ")
+        document.forms["admin_signup_form"]["adminid"].value = "14/CE/";
+    if(dpt=="cse")
+        document.forms["admin_signup_form"]["adminid"].value = "14/CSE/";
+    if(dpt=="ee")
+        document.forms["admin_signup_form"]["adminid"].value = "14/EE/";
+    if(dpt=="ece")
+        document.forms["admin_signup_form"]["adminid"].value = "14/ECE/";
+    if(dpt=="it")
+        document.forms["admin_signup_form"]["adminid"].value = "14/IT/";
+    if(dpt=="me")
+        document.forms["admin_signup_form"]["adminid"].value = "14/ME/";
+    if(dpt=="mme")
+        document.forms["admin_signup_form"]["adminid"].value = "14/MME/";
+
+}
--- registration/validation-student.js
+++ registration/validation-student.js
@@ -25,3 +25,39 @@ function validateForm() {
         return false;
     }
 }
+
+function getAge(){
+	var dateString = document.forms["student_signup_form"]["dob"].value;
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    document.forms["student_signup_form"]["age"].value = age;
+}
+
+function getReg() {
+	var dpt = document.forms["student_signup_form"]["list"].value;
+	if(dpt=="bt")
+		document.forms["student_signup_form"]["regno"].value = "14/BT/";
+	if(dpt=="che")
+		document.forms["student_signup_form"]["regno"].value = "14/CHE/";
+	if(dpt=="civ")
+		document.forms["student_signup_form"]["regno"].value = "14/CE/";
+	if(dpt=="cse")
+		document.forms["student_signup_form"]["regno"].value = "14/CSE/";
+	if(dpt=="ee")
+		document.forms["student_signup_form"]["regno"].value = "14/EE/";
+	if(dpt=="ece")
+		document.forms["student_signup_form"]["regno"].value = "14/ECE/";
+	if(dpt=="it")
+		document.forms["student_signup_form"]["regno"].value = "14/IT/";
+	if(dpt=="me")
+		document.forms["student_signup_form"]["regno"].value = "14/ME/";
+	if(dpt=="mme")
+		document.forms["student_signup_form"]["regno"].value = "14/MME/";
+
+}
